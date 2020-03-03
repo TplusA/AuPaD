7b459676770b3acadf99cd78a853b4ebcb492123
Robert Tiemann <r.tiemann@ta-hifi.com>
2020-03-03 11:35:31 +0100

    diagrams.sh: Output .pdf files as well.
    Change-Id: I5dcb5dec40f85e7bcaa52f2f3c4abc384b256b53

--- tools/diagrams.sh
+++ tools/diagrams.sh
@@ -9,4 +9,8 @@ do
     OUT="model_${DEV}.ps"
     echo "Model: ${DEV} -> ${OUT}"
     "${SHOWPATH}" --device-id "${DEV}" --dot - documentation/models.json | dot -o "${OUT}" -Tps
+
+    OUT="model_${DEV}.pdf"
+    echo "Model: ${DEV} -> ${OUT}"
+    "${SHOWPATH}" --device-id "${DEV}" --dot - documentation/models.json | dot -o "${OUT}" -Tpdf
 done
