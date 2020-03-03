af7e7fe2d626a1529c8d3837c334a3e1d091fd48
Robert Tiemann <r.tiemann@ta-hifi.com>
2020-03-03 09:03:28 +0100

    diagrams.sh: Generate PostScript diagrams for all models.
    Change-Id: Ibf9acc193372eeee46da1b3f9a7ac0b45a845524

---
+++ tools/diagrams.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+set -eu
+
+SHOWPATH=$(dirname "$0")/show_path.py
+
+for DEV in $(jq '.all_devices | keys[]' documentation/models.json | sed 's/"//g')
+do
+    OUT="model_${DEV}.ps"
+    echo "Model: ${DEV} -> ${OUT}"
+    "${SHOWPATH}" --device-id "${DEV}" --dot - documentation/models.json | dot -o "${OUT}" -Tps
+done
